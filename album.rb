15e3b33f599b3a567ebe782f3b1cfb829438ab2a
jli063 <jli063@rsdmo.org>
2018-06-22 19:07:04 +0000

    automatically backed up by learn

--- album.rb
+++ album.rb
@@ -25,7 +25,9 @@ end
 taylor_swift = Album.new("Reputation", "2017")
 shawn_mendes= Album.new("Shawn Mendes","2018")
 drake = Album.new("Scorpion","2018")
+
+puts "#{album.count}"
 #puts "#{taylor_swift.name_of_album} was made in #{taylor_swift.date_of_album}"
 
 
-puts "#{shawn_mendes.name_of_album} was made in #{shawn_mendes.date_of_album}"
+#puts "#{shawn_mendes.name_of_album} was made in #{shawn_mendes.date_of_album}"
