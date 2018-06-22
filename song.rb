3a7403f0aea408623ca593171762babe90e9de39
jli063 <jli063@rsdmo.org>
2018-06-22 19:31:29 +0000

    automatically backed up by learn

--- song.rb
+++ song.rb
@@ -6,4 +6,20 @@ class Song
     @name=name
   end
   
+  def gets_genre
+    @genre
+  end
+  
+  def gets_artist
+    @artist
+  end
+  
+  def gets_name
+    @name
+  end
+  
 end
+
+jon_song = Song.new("Hippity Hoppity", "Jon Mendes", "Jon's Way")
+
+puts jon_song.gets_genre
