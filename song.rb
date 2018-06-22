af6d5319262c1f468f810e647ceb24d3364db8ac
jli063 <jli063@rsdmo.org>
2018-06-22 19:53:41 +0000

    automatically backed up by learn

--- song.rb
+++ song.rb
@@ -2,12 +2,17 @@ class Song
   
   attr_accessor :genre,:artist,:name #replaces our gets_genre, gets_artist, and gets_name methods
   
+  @@count = 0
+
   def initialize(genre,artist,name)
     @genre=genre
     @artist=artist
     @name=name
   end
 
+  def self.get_count
+    @@count
+  end
 end
 
 jon_song = Song.new("Hippity Hoppity", "Jon Mendes", "Jon's Way")
