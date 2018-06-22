28efb14fc0ad4d2a651a8bc22ede638acbd61bfa
jli063 <jli063@rsdmo.org>
2018-06-22 19:37:52 +0000

    automatically backed up by learn

--- song.rb
+++ song.rb
@@ -12,7 +12,7 @@ end
 
 jon_song = Song.new("Hippity Hoppity", "Jon Mendes", "Jon's Way")
 
-puts jon_song.gets_genre
+puts jon_song.genre
 puts "Wow, what a great choice!"
-puts jon_song.gets_name
-puts jon_song.gets_artist
+puts jon_song.name
+puts jon_song.artist
